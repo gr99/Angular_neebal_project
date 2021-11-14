9f9350a1a10c41daee94dfff5bdb26305b231ff4
gr99 <49570196+gr99@users.noreply.github.com>
2021-11-15 01:38:35 +0530

    Add files via upload

---
+++ 12_AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.example.appwithsettings;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+
+}
